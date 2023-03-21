52dbae9ca1853f5e2e397b512831d2d0f7e2d513
namu2267 <namu2267@gmail.com>
2023-03-21 11:57:16 +0900

    대문자와소문자,toUpperCase()/toLowerCase()

---
+++ UpperCaseLowerCase.js
@@ -0,0 +1,19 @@
+// 대문자와 소문자
+
+// 문제 설명
+
+// 문자열 my_string이 매개변수로 주어질 때, 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
+
+// map(), charCodeAt(), toUpperCase(), toLowerCase()
+function solution(my_string) {
+  return [...my_string].map((a) =>
+    a.charCodeAt() < 97 ? a.toLowerCase() : a.toUpperCase()
+  );
+}
+
+// map(), toUpperCase(), toLowerCase()
+function solution(my_string) {
+  return [...my_string].map((a) =>
+    a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()
+  );
+}
