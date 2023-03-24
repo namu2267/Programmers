c4657f262fc6a45ee24c5ee1a7b834405982c5c1
namu2267 <namu2267@gmail.com>
2023-03-24 16:18:52 +0900

    n의배수고르기,filter()

---
+++ MultipleN.js
@@ -0,0 +1,10 @@
+// n의 배수 고르기
+
+// 문제 설명
+
+// 정수 n과 정수 배열 numlist가 매개변수로 주어질 때, numlist에서 n의 배수가 아닌 수들을 제거한 배열을 return하도록 solution 함수를 완성해주세요.
+
+//filter()
+function solution(n, numlist) {
+  return numlist.filter((a) => a % 3 === 0);
+}
