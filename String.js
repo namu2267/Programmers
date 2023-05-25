987a6e4e9e359841044467353e7125886490d258
namu2267 <namu2267@gmail.com>
2023-05-25 16:23:49 +0900

    문자열로 변환, String()

---
+++ String.js
@@ -0,0 +1,16 @@
+// 문자열로 변환
+
+// 문제 설명
+// 정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+
+// String()을 사용하여 문자열로 변환
+function solution(n) {
+  return String(n)
+}
+
+
+//다른 방법
+// +""
+// toString()
+
