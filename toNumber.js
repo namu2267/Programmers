420b59d057e8c1ce8029fe8b408f0ae5f0285fa8
namu2267 <namu2267@gmail.com>
2023-05-26 01:51:25 +0900

    문자열을 정수로 변환하기,Number()

---
+++ toNumber.js
@@ -0,0 +1,17 @@
+// 문자열을 정수로 변환하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181848
+
+// 문제 설명
+// 숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+//Number()
+function solution(n_str) {
+  return Number(n_str);
+}
+
+// // 공부내용 정리 블로그
+// https://hihiha2.tistory.com/78
+// number로 변환 메서드
+// 1. Number(): 문자열이나 다른 값을 Number 타입으로 변환
+// 2. +
+// 3. parseInt()
