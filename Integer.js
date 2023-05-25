07b65be4b9d0ebac900b71015b71f290a3ef8a90
namu2267 <namu2267@gmail.com>
2023-05-26 01:26:09 +0900

    정수 부분,Math.floor()

---
+++ Integer.js
@@ -0,0 +1,17 @@
+// 정수 부분
+
+// 문제 설명
+// https://school.programmers.co.kr/learn/courses/30/lessons/181850
+// 실수 flo가 매개 변수로 주어질 때, flo의 정수 부분을 return하도록 solution 함수를 완성해주세요.
+
+// Math.floor()
+function solution(flo) {
+  return Math.floor(flo);
+}
+
+// 공부내용 정리 블로그
+// https://hihiha2.tistory.com/77
+// 숫자를 정수로 만드는 방법
+// 1. Math.floor(): 항상 내림하고 주어진 숫자보다 작거나 같은 가장 큰 정수를 반환
+// 2. Math.trunc: 소수 자릿수를 제거하여 숫자의 정수 부분을 반환
+// 3. parseInt(): 문자열 인자를 파싱하여 정수를 반환
