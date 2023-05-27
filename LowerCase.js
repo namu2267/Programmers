36b9be46fa06d746662870dd0f00d67a2864e6c8
namu2267 <namu2267@gmail.com>
2023-05-28 02:06:45 +0900

    소문자로 바꾸기,toLowerCase()

---
+++ LowerCase.js
@@ -0,0 +1,13 @@
+// 소문자로 바꾸기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181876
+
+// 문제 설명
+// 알파벳으로 이루어진 문자열 myString이 주어집니다. 모든 알파벳을 소문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
+
+//toLowerCase()
+function solution(myString) {
+  return myString.toLowerCase();
+}
+
+//toLowerCase()
+// 알고 있는 메서드여서 바로 풀었던 문제
