bd191c3052b1332e2735ff5876c701aad9fb7fcd
namu2267 <namu2267@gmail.com>
2023-06-04 00:02:41 +0900

    주사위 게임1,if문/Math.abs()

---
+++ DiceGame1.js
@@ -0,0 +1,25 @@
+// 주사위 게임 1
+// https://school.programmers.co.kr/learn/courses/30/lessons/181839
+
+// 문제 설명
+// 1부터 6까지 숫자가 적힌 주사위가 두 개 있습니다. 두 주사위를 굴렸을 때 나온 숫자를 각각 a, b라고 했을 때 얻는 점수는 다음과 같습니다.
+
+// a와 b가 모두 홀수라면 a2 + b2 점을 얻습니다.
+// a와 b 중 하나만 홀수라면 2 × (a + b) 점을 얻습니다.
+// a와 b 모두 홀수가 아니라면 |a - b| 점을 얻습니다.
+// 두 정수 a와 b가 매개변수로 주어질 때, 얻는 점수를 return 하는 solution 함수를 작성해 주세요.
+
+// if문, Math.abs()
+function solution(a, b) {
+  if (a % 2 === 1 && b % 2 === 1) {
+    return a ** 2 + b ** 2;
+  } else if ((a % 2 === 1 && b % 2 === 0) || (a % 2 === 0 && b % 2 === 1)) {
+    return 2 * (a + b);
+  } else {
+    return Math.abs(a - b);
+  }
+}
+
+// 공부내용 정리 블로그
+// https://hihiha2.tistory.com/100
+// Math.abs(): 주어진 숫자의 절대값을 반환
