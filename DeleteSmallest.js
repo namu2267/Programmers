cca6487b6daa9f8c4e925f2ff8fbc8a438eb9abb
namu2267 <namu2267@gmail.com>
2023-09-02 23:59:51 +0900

    제일 작은 수 제거하기

---
+++ DeleteSmallest.js
@@ -0,0 +1,18 @@
+/* 제일 작은 수 제거하기
+https://school.programmers.co.kr/learn/courses/30/lessons/12935
+
+문제 설명
+정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요. 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+*/
+
+function solution(arr) {
+  let min = Math.min(...arr);
+  let index = arr.findIndex((v) => v === min);
+
+  arr.splice(index, 2);
+  return arr.length ? arr : [-1];
+}
+
+/* 공부내용 정리 블로그
+https://hihiha2.tistory.com/166
+*/
